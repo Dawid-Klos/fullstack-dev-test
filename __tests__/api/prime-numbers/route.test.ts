32ab05f3089668c1010a37db676dc5357d1fe494
Dawid Klos <klosdawid12@gmail.com>
2024-02-23 11:00:26 +0000

    test: api/prime-numbers endpoint for non-numerical input

--- __tests__/api/prime-numbers/route.test.ts
+++ __tests__/api/prime-numbers/route.test.ts
@@ -20,3 +20,39 @@ const mockApiRequest = (value: string) => {
 
   return nextReq;
 };
+
+describe("GET /api/prime-numbers", () => {
+  it("should return 400 status if value is not a number", async () => {
+    const req: NextRequest = mockApiRequest("abc");
+    const res: NextResponse = await GET(req);
+
+    expect(res.status).toBe(400);
+  });
+
+  it("should return empty array if value is not a number", async () => {
+    const req: NextRequest = mockApiRequest("abc");
+    const res: NextResponse = await GET(req);
+
+    const jsonResponse = await res.json();
+
+    expect(jsonResponse.data).toEqual([]);
+  });
+
+  it("should return error if value is not a number", async () => {
+    const req: NextRequest = mockApiRequest("abc");
+    const res: NextResponse = await GET(req);
+
+    const jsonResponse = await res.json();
+
+    expect(jsonResponse.error).toEqual("Expected numerical value not a string");
+  });
+
+  it("should return message if value is not a number", async () => {
+    const req: NextRequest = mockApiRequest("abc");
+    const res: NextResponse = await GET(req);
+
+    const jsonResponse = await res.json();
+
+    expect(jsonResponse.message).toEqual("Invalid value");
+  });
+});
