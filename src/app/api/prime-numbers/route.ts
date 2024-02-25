633d7a983631aa9eed1cd3074bb7560f19101651
Dawid Klos <klosdawid12@gmail.com>
2024-02-25 08:18:57 +0000

    refactor: prime-number route to use handleError function

--- src/app/api/prime-numbers/route.ts
+++ src/app/api/prime-numbers/route.ts
@@ -2,60 +2,38 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 import { findPrimeNumbersTo } from "utils/primeNumbers";
+import { handleError } from "utils/errorHandler";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
+  const value = searchParams.get("value");
 
-  if (!searchParams.has("value") || searchParams.get("value") === "") {
-    return NextResponse.json(
-      {
-        message: "Bad request",
-        error: "Expected a value, but none was provided",
-        statusCode: 400,
-        data: [],
-      },
-      { status: 400 }
+  if (!value || value === "") {
+    return handleError(
+      "Bad request",
+      "Expected a value, but none was provided",
+      400
     );
   }
 
-  const x: number = Number(searchParams.get("value"));
+  const x: number = Number(value);
 
-  if (isNaN(x)) {
-    const xType = typeof searchParams.get("value");
-
-    return NextResponse.json(
-      {
-        message: "Bad request",
-        error: `Expected numerical value not a ${xType}`,
-        statusCode: 400,
-        data: [],
-      },
-      { status: 400 }
-    );
-  }
-
-  if (!Number.isInteger(x)) {
-    return NextResponse.json(
-      {
-        message: "Bad request",
-        error: `Expected an integer not a float`,
-        statusCode: 400,
-        data: [],
-      },
-      { status: 400 }
-    );
-  }
-
-  if (x < 2) {
-    return NextResponse.json(
-      {
-        message: "Invalid value",
-        error: "The smallest prime number is 2",
-        statusCode: 422,
-        data: [],
-      },
-      { status: 422 }
-    );
+  switch (true) {
+    case isNaN(x):
+      const xType = typeof value;
+      return handleError(
+        "Bad request",
+        `Expected numerical value not a ${xType}`,
+        400
+      );
+    case !Number.isInteger(x):
+      return handleError("Bad request", `Expected an integer not a float`, 400);
+    case x < 2:
+      return handleError(
+        "Invalid value",
+        "The smallest prime number is 2",
+        422
+      );
   }
 
   const primeNumbers = findPrimeNumbersTo(x);
