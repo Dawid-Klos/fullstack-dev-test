332859b9b94725afb67f7fd1d1f36bb57e469dc7
Dawid Klos <klosdawid12@gmail.com>
2024-02-26 20:47:32 +0000

    feat: add mocking API call with pagination values

--- __mocks__/mockApiRequest.ts
+++ __mocks__/mockApiRequest.ts
@@ -16,3 +16,23 @@ export const mockApiRequest = (value: string) => {
 
   return nextReq;
 };
+
+export const mockApiRequestWithPagination = (
+  value: string,
+  limit: string,
+  page: string
+) => {
+  const searchParams = new URLSearchParams({ value, limit, page });
+
+  const { req } = createRequest({
+    method: "GET",
+    headers: { "content-type": "application/json" },
+  });
+
+  const nextReq: NextRequest = {
+    ...req,
+    nextUrl: { searchParams },
+  };
+
+  return nextReq;
+};
