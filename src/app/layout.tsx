0cf92339d20fc2954dc607cff2f5fe5402695726
Dawid Klos <klosdawid12@gmail.com>
2024-02-27 19:27:03 +0000

    feat: use main tag in the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.variable} ${poppins.variable}`}>
-        {children}
+        <main>{children}</main>
       </body>
     </html>
   );
