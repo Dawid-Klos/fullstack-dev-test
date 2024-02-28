413ed3a2bc874f61e89cf4ee2bec84e604c484ee
Dawid Klos <klosdawid12@gmail.com>
2024-02-28 13:19:49 +0000

    feat: use Search component on search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
+import SearchSection from "components/Search";
+
 import bgBlurs from "assets/home-bg-blurs.svg";
 import styles from "./page.module.scss";
 
 export default function Search() {
   return (
     <section className={styles.section}>
+      <SearchSection />
       <Image
         className={styles.bgBlurs}
         src={bgBlurs}
