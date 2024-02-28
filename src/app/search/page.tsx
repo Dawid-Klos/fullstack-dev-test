b614c0aa511ad9070d565257eab77e31f26f698a
Dawid Klos <klosdawid12@gmail.com>
2024-02-28 17:30:11 +0000

    feat: add SearchResult to Search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import SearchSection from "components/Search";
+import SearchResult from "components/SearchResult";
 
 import bgBlurs from "assets/home-bg-blurs.svg";
 import styles from "./page.module.scss";
@@ -9,6 +10,7 @@ export default function Search() {
   return (
     <section className={styles.section}>
       <SearchSection />
+      <SearchResult />
       <Image
         className={styles.bgBlurs}
         src={bgBlurs}
