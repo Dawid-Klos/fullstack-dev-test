b0b752743de9cf7c856f8ec42002f712597fbebf
Dawid Klos <klosdawid12@gmail.com>
2024-02-28 18:33:14 +0000

    feat: add className optional prop to Button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -3,6 +3,7 @@ import styles from "./Button.module.scss";
 type Props = {
   type?: "submit" | "button";
   disabled?: boolean;
+  className?: string;
   children: string;
   onClick?: () => void;
 };
@@ -10,12 +11,13 @@ type Props = {
 export const Button = ({
   type = "button",
   disabled = false,
+  className,
   onClick,
   children,
 }: Props) => {
   return (
     <button
-      className={styles.button}
+      className={`${styles.button} ${className}`}
       type={type}
       disabled={disabled}
       onClick={onClick}
